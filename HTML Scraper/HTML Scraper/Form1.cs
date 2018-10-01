968e1854df58640fd9f0ac9bf412ba27f4dcb870
CoderXYZ1 <43748934+CoderXYZ1@users.noreply.github.com>
2018-10-01 13:45:13 -0700

    Source ;)

---
+++ HTML Scraper/HTML Scraper/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace HTML_Scraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = client.DownloadString(textBox1.Text);
+                richTextBox1.Text = htmlCode;
+            }
+        }
+    }
+}
